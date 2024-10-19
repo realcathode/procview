d2fe64574ff9e1e7bbbb22bb7d9ccc1e64a64c12
realcathode <ion.miron@tutanota.com>
2024-10-19 20:22:32 +0300

    Add active processes display

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ use sysinfo::System;
 
 const BYTES_PER_MIB: u64 = 1_048_576;
 
+#[derive(Debug)]
+struct Process {
+    pid: u32,
+    uid: u32,
+    cpu: f32,
+    mem: u64,       
+}           
+
+impl Process {
+    fn new(pid: u32, uid: u32, cpu: f32, mem: u64) -> Self {
+        Self {
+            pid,
+            uid,
+            cpu,
+            mem,
+        }
+    }
+}
+
 fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -11,4 +30,15 @@ fn main() {
     println!("Swap (MiB): {}", sys.total_swap() / BYTES_PER_MIB);
     println!("Used swap (MiB): {}", sys.used_swap() / BYTES_PER_MIB);   
     
+    let mut procs: Vec<Process> = Vec::new();
+    for (pid, process) in sys.processes() {
+        // Check if user_id is available
+        if let Some(uid ) = process.user_id() {
+            let nproc = Process::new(pid.as_u32(), **uid, process.cpu_usage(), process.memory() / BYTES_PER_MIB);
+            procs.push(nproc);
+        }
+    }
+    for p in procs {
+        println!("{:?}", p);
+    }
 }
