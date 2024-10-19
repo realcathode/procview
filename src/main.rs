c7aa4724822b6f5fa4b8ef066489513f29c7069c
realcathode <ion.miron@tutanota.com>
2024-10-19 20:05:40 +0300

    Add total/used memory and swap display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sysinfo::System;
+
+const BYTES_PER_MIB: u64 = 1_048_576;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    println!("Mem (MiB): {}", sys.total_memory() / BYTES_PER_MIB);
+    println!("Used mem (MiB): {}", sys.used_memory() / BYTES_PER_MIB);
+    println!("Swap (MiB): {}", sys.total_swap() / BYTES_PER_MIB);
+    println!("Used swap (MiB): {}", sys.used_swap() / BYTES_PER_MIB);   
+    
 }
